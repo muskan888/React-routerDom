a2a6a68e03eb7af9508a48bd3d563cc2f42df6f5
muskan888 <dhingramuskan2001@gmail.com>
2024-06-03 10:48:33 -0400

    lazy loading

--- router-dom-practice/src/App.jsx
+++ router-dom-practice/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useState,lazy,Suspense } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter ,Routes,Route} from 'react-router-dom'
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
 import { useNavigate } from 'react-router-dom'
-
+const Dashboard = lazy(()=> import('./components/Dashboard'))
+const Landing = lazy(()=> import('./components/Landing'))
 function App() {
   return (
-    <div>
+    <div>  
 
     <BrowserRouter>
     <Appbar/>
@@ -17,8 +16,9 @@ function App() {
 
    
     <Routes>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/" element={<Landing/>}/>
+   
+      <Route path="/dashboard" element={ <Suspense fallback={<div>Loading...</div>}><Dashboard/></Suspense>}/>
+      <Route path="/" element={ <Suspense fallback={<div>Loading...</div>}><Landing/></Suspense>}/>
     </Routes>
     
     
@@ -28,7 +28,6 @@ function App() {
 }
 function Appbar(){
   const navigate=useNavigate();
-
   return  <div>
   <button onClick={()=>{
     navigate("/");
